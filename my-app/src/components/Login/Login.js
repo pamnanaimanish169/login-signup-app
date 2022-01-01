77671c88b2a0358dbec736888f8c6534f0af3a7f
manish <manishpamnani169@gmail.com>
2022-01-01 12:22:02 +0530

    Setup Git, Install Bootstrap, Define routes

---
+++ my-app/src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                Login Works!
+            </div>
+        )
+    }
+}
+
+export default Login;
---
+++ my-app/src/components/Register/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Register.css';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Registration Works!
+            </div>
+        )
+    }
+}
+
+export default Register;
