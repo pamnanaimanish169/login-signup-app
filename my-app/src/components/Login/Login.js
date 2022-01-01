4847bd8277968b5091fe89a1d875de6e1f0fc0e1
manish <manishpamnani169@gmail.com>
2022-01-01 18:19:40 +0530

    Added Validation for Login Page

--- my-app/src/components/Login/Login.js
+++ my-app/src/components/Login/Login.js
@@ -2,102 +2,101 @@ import React from 'react';
 import './Login.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import {FormErrors} from '../../FormErrors';
+import { FormErrors } from '../../FormErrors';
 
 class Login extends React.Component {
     constructor(props) {
         super(props);
-
-        this.handleSubmit = this.handleSubmit.bind(this);
-
         this.state = {
-            email : '',
-            password : '',
-            // for enabling/disabling submit button
-            formErrors : {email : '', password : ''},
-            // individual fields validation
-            emailValid : false,
-            passwordValid : false,
-            // Whole form
-            formValid : false
+            email: '',
+            password: '',
+            formErrors: { email: '', password: '' },
+            emailValid: false,
+            passwordValid: false,
+            formValid: false
         };
-    }
 
-    handleSubmit(event) {
-        console.log('submit event', event);
-
-        event.preventDefault();
+        this.handleSubmit   =   this.handleSubmit.bind(this);
+        this.handleUserInput    =   this.handleUserInput.bind(this);
     }
 
     handleUserInput(e) {
         const name = e.target.name;
         const value = e.target.value;
-
-        // callback function for setState which will validate as per the validation rules we specify in it.
-        this.setState({[name] : value}, () => { this.validateField(name, value) });
+        this.setState({ [name]: value },
+            () => { this.validateField(name, value) });
     }
 
-    // define the callback function
     validateField(fieldName, value) {
         let fieldValidationErrors = this.state.formErrors;
-        let emailValid  =   this.state.emailValid;
-        let passwordValid   =   this.state.passwordValid;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
 
-        switch(fieldName) {
-
-            // validations for as per the fields,
-            // to add more fieds just add the field with the case keyword
-            // and mention the regex
-            // regex for email format i.e. @,.com,etc
-            case 'email' : 
-                emailValid  =   value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.email =   emailValid  ?   ''  :   'is invalid';
+        switch (fieldName) {
+            case 'email':
+                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                fieldValidationErrors.email = emailValid ? '' : 'is invalid';
                 break;
-            case 'password' :
-                passwordValid   =   value.length >= 6;
-                fieldValidationErrors.password  =   passwordValid   ?   ''  :   'too short';
+            case 'password':
+                passwordValid = value.length >= 6;
+                fieldValidationErrors.password = passwordValid ? '' : 'too short';
                 break;
-            // No condition for default as no fields here in default
-            // it will match a single field always
-            default :
+            default:
                 break;
         }
 
-        // this.calidateForm -> callback funtion while validating the fields
         this.setState({
-            formErrors   :   fieldValidationErrors,
-            emailValid  :   emailValid,
-            passwordValid   :   passwordValid
+            formErrors: fieldValidationErrors,
+            emailValid: emailValid,
+            passwordValid: passwordValid
         }, this.validateForm)
     }
 
-    validateForm()  {
-        // we need to include more field in here function if more field is included in the HTML form
-        this.setState({formValid    :   this.state.emailValid   &&  this.state.passwordValid    });
+    validateForm() {
+        this.setState({ formValid: this.state.emailValid && this.state.passwordValid });
+    }
+
+    errorClass(error) {
+        return (error.length === 0 ? '' : 'has-error');
+    }
+
+    handleSubmit(event) {
+        // TO prevent the page from reloading
+        event.preventDefault();
     }
 
     render() {
         return (
             <div className='container'>
+                <div>
+                    Login
+                </div>
                 <Form>
-                    <FormErrors formErrors={this.state.formErrors}>
+                    <div className="panel panel-default">
+                        <FormErrors formErrors={this.state.formErrors} />
+                    </div>
 
-                    </FormErrors>
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" value={this.state.email} onChange={(event) => this.handleUserInput(event)}/>
+                    <Form.Group className={`mb-3 form-group ${this.errorClass(this.state.formErrors)}`}>
+                        <Form.Control 
+                        name="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleUserInput}
+                        />
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={(event) => this.handleUserInput(event)}/>
+                    <Form.Group className={`mb-3 form-group ${this.errorClass(this.state.formErrors)}`}>
+                        <Form.Control
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleUserInput}
+                        />
+
                     </Form.Group>
 
-                    {/* Disabled attribute set to validity of the form */}
-                    <Button disabled={!this.state.formValid} variant="primary" type="submit" onClick={this.handleSubmit}>
-                        Submit
-                    </Button>
+                    <Button variant="primary" disabled={!this.state.formValid}>Sign Up</Button>
                 </Form>
             </div>
         )
